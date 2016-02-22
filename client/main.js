db460de207dc8017ee1853cc2689a37f7b572b63
jmcshane <jmcshan1@gmail.com>
2016-02-22 17:03:40 -0600

    Step 3: Refactoring simple-todos-react.jsx

---
+++ client/main.js
@@ -0,0 +1,10 @@
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
+});
+
+Meteor.subscribe("tasks");
+
+Meteor.startup(function () {
+  // Use Meteor.startup to render the component after the page is ready
+  React.render(<App />, document.getElementById("render-target"));
+});
